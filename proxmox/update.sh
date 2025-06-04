4b3779d185c7ab9783c1e26be76c0b76a0ded263
Liu <12598936+liubsp@users.noreply.github.com>
2025-06-04 22:36:40 +0200

    Refactor update script to remove Telepush integration and replace with notification to the root's email adress

--- proxmox/update.sh
+++ proxmox/update.sh
@@ -1,38 +1,22 @@
 #!/bin/bash
-# Usage: ./update.sh <telepush_url>
+# Usage: ./update.sh
 
-# Check if Telepush URL is provided
-if [ -z "$1" ]; then
-    echo "Error: Telepush URL not provided."
-    echo "Usage: $0 <telepush_url>"
-    exit 1
-fi
-
-TELEPUSH_URL="$1"
+ROOT_EMAIL=$(sudo pveum user list | awk '/root@pam/ { print $5 }')
 LOG_FILE="/tmp/pve_upgrade.log"
 TIMEOUT_DURATION="5m"
 
-# Commands to run: update, upgrade (with auto-yes), autoremove (with auto-yes), and capture exit code
-UPDATE_COMMANDS="(sudo /usr/bin/pveupdate && yes y | sudo /usr/bin/pveupgrade && yes y | sudo apt autoremove; echo \"Status code: \$?\")"
+# Commands to run: update, upgrade (with auto-yes), autoremove (with auto-yes)
+UPDATE_COMMANDS='sudo /usr/bin/pveupdate && yes y | sudo /usr/bin/pveupgrade && yes y | sudo apt autoremove'
 
-# Run commands with timeout, redirecting all output to the log file
 timeout "$TIMEOUT_DURATION" bash -c "$UPDATE_COMMANDS" > "$LOG_FILE" 2>&1
+COMMAND_EXIT_CODE=$?
 
-# Check if the command completed within the timeout
-if [ $? -eq 0 ]; then
-    # Origin string for the notification message
-    ORIGIN="PVE update cron at $HOSTNAME"
-
-    # Format log content into JSON and send via curl
-    jq -n \
-       --rawfile f "$LOG_FILE" \
-       --arg origin "$ORIGIN" \
-       '{ origin: $origin, text: $f }' | \
-    curl -X POST \
-         -H "Content-Type: application/json" \
-         -d @- \
-         "$TELEPUSH_URL"
+if [ $COMMAND_EXIT_CODE -eq 0 ]; then
+    STATUS="succeeded"
 else
-    # Handle timeout or command failure
-    echo "Command timed out or failed. Check $LOG_FILE for details."
+    STATUS="failed"
 fi
+
+SUBJECT="PVE update cron at $HOSTNAME $STATUS"
+BODY=$(cat "$LOG_FILE")
+echo "$BODY" | mail -s "$SUBJECT" "$ROOT_EMAIL"
