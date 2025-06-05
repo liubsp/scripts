e0e1ef524589318e8cd72828d314d3b420606ebd
Liu <12598936+liubsp@users.noreply.github.com>
2025-06-05 23:47:16 +0200

    Require sudo for update.sh script

--- proxmox/update.sh
+++ proxmox/update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Usage: ./update.sh
+# Usage: sudo ./update.sh
 
-ROOT_EMAIL=$(sudo pveum user list | awk '/root@pam/ { print $5 }')
+ROOT_EMAIL=$(pveum user list | awk '/root@pam/ { print $5 }')
 LOG_FILE="/tmp/pve_upgrade.log"
 TIMEOUT_DURATION="5m"
 
 # Commands to run: update, upgrade (with auto-yes), autoremove (with auto-yes)
-UPDATE_COMMANDS='sudo /usr/bin/pveupdate && yes y | sudo /usr/bin/pveupgrade && yes y | sudo apt autoremove'
+UPDATE_COMMANDS='/usr/bin/pveupdate && yes y | /usr/bin/pveupgrade && yes y | apt autoremove'
 
 timeout "$TIMEOUT_DURATION" bash -c "$UPDATE_COMMANDS" > "$LOG_FILE" 2>&1
 COMMAND_EXIT_CODE=$?
